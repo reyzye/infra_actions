f9e400749f87489a52e1ef05a91bd6f6a8c7dbce
reyzye <reyzye@yandex.ru>
2023-04-05 14:54:20 +0700

    test-3

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Матусан Станислав Леонидович, покупай джетту, заебал!')
+    return HttpResponse('Матусан Станислав Леонидович, покупай джетту, заебал')
 
 
 def second_page(request):
